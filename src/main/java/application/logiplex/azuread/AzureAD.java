0ca0f75bfb42ef82075925a05d408ce1981941c3
saaronsas <sasaronsa@gmail.com>
2022-04-19 10:17:14 +0200

    createDirectories

--- src/main/java/application/logiplex/azuread/AzureAD.java
+++ src/main/java/application/logiplex/azuread/AzureAD.java
@@ -6,6 +6,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import static application.Config.*;
+import static application.logiplex.ad.Config.VARIABLE_APP_NAME;
+import static application.logiplex.ad.Config.VARIABLE_CERTIFICATION_GROUP_NAME;
+import static application.logiplex.ad.Config.VARIABLE_CERTIFICATION_NAME;
+import static application.logiplex.ad.Config.VARIABLE_SCOPE_NAME;
+import static application.logiplex.ad.Config.VARIABLE_TASK_LIST;
+import static application.logiplex.ad.Config.VARIABLE_TASK_NAME;
+import static application.logiplex.ad.Config.VARIABLE_WORKGROUP_NAME;
 import static application.logiplex.azuread.Config.*;
 
 public class AzureAD extends BaseApplication {
@@ -13,6 +20,9 @@ public class AzureAD extends BaseApplication {
     private static String scope = "";
     private static String name = "";
     private static String applicationName = "";
+    private static String certDefName = "";
+    private static String workgroupName = "";
+    private static String certGroupName = "";
 
     private static String taskPrefix[] = {"-TaskDefinition-AccountAggregation-", "-TaskDefinition-GroupAggregation-", "-TaskDefinition-FullAggregation-"};
     private static ArrayList<String> taskLists = new ArrayList();
@@ -42,6 +52,7 @@ public class AzureAD extends BaseApplication {
             applicationName = getApplicationName(name);
             saveFile(createDirectory(OUTPUT_XML_FILE_NAME + applicationName) + applicationName + XML_EXTENSION, readFile(chargeFile(INPUT_XML_LOGILPEX_APPLICATION_TEMPLATE), ""));
             createTasks(new String[]{INPUT_XML_ACCOUNT_AGGREGATION_TEMPLATE, INPUT_XML_GROUP_AGGREGATION_TEMPLATE, INPUT_XML_FULL_AGGREGATION_TEMPLATE});
+            createCertifications();
         }
 
     }
@@ -60,10 +71,18 @@ public class AzureAD extends BaseApplication {
 
             if (line.contains(VARIABLE_APP_NAME)) {
                 line = line.substring(0, line.indexOf(VARIABLE_START)) + applicationName + line.substring(line.indexOf(VARIABLE_END) + 2);
+            } else if (line.contains(VARIABLE_SCOPE_NAME)) {
+                line = line.substring(0, line.indexOf(VARIABLE_START)) + scope + line.substring(line.indexOf(VARIABLE_END) + 2);
             } else if (line.contains(VARIABLE_TASK_NAME)) {
                 line = line.substring(0, line.indexOf(VARIABLE_START)) + temporalName + line.substring(line.indexOf(VARIABLE_END) + 2);
             } else if (line.contains(VARIABLE_TASK_LIST)) {
                 line = line.substring(0, line.indexOf(VARIABLE_START)) + taskLists.toString().substring(taskLists.toString().indexOf("[") + 1, taskLists.toString().indexOf("]")) + line.substring(line.indexOf(VARIABLE_END) + 2);
+            } else if (line.contains(VARIABLE_CERTIFICATION_NAME)) {
+                line = line.substring(0, line.indexOf(VARIABLE_START)) + certDefName + line.substring(line.indexOf(VARIABLE_END) + 2);
+            } else if (line.contains(VARIABLE_WORKGROUP_NAME)) {
+                line = line.substring(0, line.indexOf(VARIABLE_START)) + workgroupName + line.substring(line.indexOf(VARIABLE_END) + 2);
+            } else if (line.contains(VARIABLE_CERTIFICATION_GROUP_NAME)) {
+                line = line.substring(0, line.indexOf(VARIABLE_START)) + certGroupName + line.substring(line.indexOf(VARIABLE_END) + 2);
             }
 
             data += line + "\n";
@@ -80,9 +99,23 @@ public class AzureAD extends BaseApplication {
     private static void createTasks(String[] tasks) throws IOException {
         for (int i = 0; i < tasks.length; i++) {
             String taskName = scope + taskPrefix[i] + name;
+
             taskLists.add(taskName);
-            saveFile(createDirectory(OUTPUT_XML_FILE_NAME + applicationName) + taskName + XML_EXTENSION, readFile(chargeFile(tasks[i]), taskName));
+            saveFile(createSubDirectory(OUTPUT_XML_FILE_NAME + applicationName, "tasks") + taskName + XML_EXTENSION, readFile(chargeFile(tasks[i]), taskName));
         }
+    }
+
+    /**
+     * Method to create certifications
+     * @throws IOException
+     */
+    private static void createCertifications() throws IOException {
+        certDefName = scope + "-CertificationDefinition-CISO " + name;
+        workgroupName = scope + "-Workgroup-" + name + "_ITOWNER";
+        certGroupName = scope + "-CertificationGroup-CISO " + name;
+        saveFile(createSubDirectory(OUTPUT_XML_FILE_NAME + applicationName, "certifications") + certDefName + XML_EXTENSION, readFile(chargeFile(INPUT_XML_CERTIFICATION_DEFINITION_TEMPLATE), certDefName));
+        saveFile(createSubDirectory(OUTPUT_XML_FILE_NAME + applicationName, "certifications") + certGroupName + XML_EXTENSION, readFile(chargeFile(INPUT_XML_CERTIFICATION_GROUP_TEMPLATE), certGroupName));
+        saveFile(createSubDirectory(OUTPUT_XML_FILE_NAME + applicationName, "certifications") + workgroupName + XML_EXTENSION, readFile(chargeFile(INPUT_XML_WORKGROUP_TEMPLATE), workgroupName));
     }
 
     /**
